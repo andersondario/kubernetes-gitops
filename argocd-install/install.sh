502efa7d3781302d3503e63713c371728d81c44d
Anderson Dario <andersondario@gmail.com>
2023-04-14 10:07:07 -0300

    Additinal Resources and Refactor (#3)

--- argocd-install/install.sh
+++ argocd-install/install.sh
@@ -1,22 +1,12 @@
 #!/bin/bash
-helm repo add kong https://charts.konghq.com
-helm repo update
-
-istioctl install --set profile=minimal -y
-
-kubectl create namespace kong-istio
 kubectl create namespace argocd
+kubectl label namespace default istio-injection=enabled
 
-kubectl label namespace kong-istio istio-injection=enabled
-kubectl label namespace argocd istio-injection=enabled
-
-helm install -n kong-istio kong-istio kong/kong
-helm install argocd ./argo-cd --namespace=argocd
+helm install -n argocd argocd ./argo-cd
 
 kubectl apply -f argocd-core-projects.yaml
-kubectl apply -f argocd-core-apps.yaml
-kubectl apply -f argocd-ingress.yaml
+kubectl apply -f argocd-core-applications.yaml
 
-sleep 60
+sleep 90
 
-kubectl -n argocd get secrets argocd-initial-admin-secret -o jsonpath='{.data.password}' | base64 -d
+kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
